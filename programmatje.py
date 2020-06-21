4cd4510651036fdb877c23431eb2dd925f070d58
katrinusdevries <34285687+katrinusdevries@users.noreply.github.com>
2020-06-21 21:57:22 +0200

    update programmatje.py met een vierde printregel

--- programmatje.py
+++ programmatje.py
@@ -1,3 +1,4 @@
 print ("Het werken met GITHUB is handig")
 print ("Dit is een fork van het programma van docentfp")
 print ("Het is maar een simpel programmatjes")
+print ("met een vierde printregel")
